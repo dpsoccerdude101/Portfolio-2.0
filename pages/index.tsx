3d06536fd390a2c51b9ff13dd084ee9bb2ab2946
Dennis Pavlyuk <37779084+dpsoccerdude101@users.noreply.github.com>
2022-08-17 15:16:24 -0400

    added animated background

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,12 @@ import { Page } from '../components';
 import Header from '../layout/Header/Header';
 import Main from '../layout/Main/Main';
 import Footer from '../layout/Footer/Footer';
+import styles from './overlay.module.sass';
 
 const Home: NextPage = () => {
 	return (
 		<Page title='Dennis Pavlyuk' description='Full-Stack Developer'>
+			<div className={styles.overlay}></div>
 			<Header />
 			<Main />
 			<Footer />
