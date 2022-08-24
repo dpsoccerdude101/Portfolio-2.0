9adab7b3b031502c70bf14dde40009c22f44f0dc
Dennis Pavlyuk <37779084+dpsoccerdude101@users.noreply.github.com>
2022-08-24 15:22:14 -0400

    astro mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,14 +9,24 @@ import cn from 'classnames';
 
 const Home: NextPage = () => {
     return (
-        <Page title='Dennis Pavlyuk' description='Full-Stack Developer'>
-{/*             <div className={cn(styles.overlay, styles.wrapper)}>
+        <Page
+            title='Dennis Pavlyuk'
+            description='Full-Stack Developer'
+            style={{
+                display: 'inline-block',
+                overflow: 'hidden',
+                position: 'relative',
+                width: '100%',
+            }}
+        >
+            {/*             <div className={cn(styles.overlay, styles.wrapper)}>
                 <div className={cn(styles.div, styles.div1)}></div>
                 <div className={cn(styles.div, styles.div2)}></div>
                 <div className={cn(styles.div, styles.div3)}></div>
                 <div className={cn(styles.div, styles.div4)}></div>
                 <div className={cn(styles.div, styles.div5)}></div>
             </div> */}
+
             <Header />
             <Main />
             <Footer />
