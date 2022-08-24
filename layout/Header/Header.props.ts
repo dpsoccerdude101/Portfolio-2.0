4d424056cdfe0301e83eb8a3b86412a0a1ff6fa8
Dennis Pavlyuk <37779084+dpsoccerdude101@users.noreply.github.com>
2022-08-24 13:58:57 -0400

    astrotheme

--- layout/Header/Header.props.ts
+++ layout/Header/Header.props.ts
@@ -1,61 +1,64 @@
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
 
-export type HeaderProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+export type HeaderProps = DetailedHTMLProps<
+    HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+>;
 
 interface socialData {
-	id: number;
-	social: string;
-	href: string;
-	icon: string;
+    id: number;
+    social: string;
+    href: string;
+    icon: string;
 }
 
 interface profileInfo {
-	id: number;
-	title: number | string;
-	desctiption: string;
+    id: number;
+    title: number | string;
+    desctiption: string;
 }
 
 export const socialData: socialData[] = [
-	{
-		id: 0,
-		social: 'Telegram',
-		href: 'https://t.me/dpsoccerdude101',
-		icon: 'ri-telegram-line',
-	},
-	{
-		id: 1,
-		social: 'Instagram',
-		href: 'https://www.instagram.com/dennis.pavlyuk/',
-		icon: 'ri-instagram-line',
-	},
-	{
-		id: 2,
-		social: 'GitHub',
-		href: 'https://github.com/dpsoccerdude101',
-		icon: 'ri-github-line',
-	},
-	{
-		id: 3,
-		social: 'Linkedin',
-		href: 'https://www.linkedin.com/in/dennis-pavlyuk-54b83610b/	',
-		icon: 'ri-linkedin-line',
-	},
+    {
+        id: 0,
+        social: 'Telegram',
+        href: 'https://t.me/dpsoccerdude101',
+        icon: 'ri-telegram-line',
+    },
+    {
+        id: 1,
+        social: 'Instagram',
+        href: 'https://www.instagram.com/dennis.pavlyuk/',
+        icon: 'ri-instagram-line',
+    },
+    {
+        id: 2,
+        social: 'GitHub',
+        href: 'https://github.com/dpsoccerdude101',
+        icon: 'ri-github-line',
+    },
+    {
+        id: 3,
+        social: 'Linkedin',
+        href: 'https://www.linkedin.com/in/dennis-pavlyuk-54b83610b/	',
+        icon: 'ri-linkedin-line',
+    },
 ];
 
 export const profileInfo: profileInfo[] = [
+    {
+        id: 0,
+        title: 'MicCheck',
+        desctiption: 'Current company',
+    },
+    {
+        id: 1,
+        title: 4,
+        desctiption: 'Projects',
+    },
 	{
-		id: 0,
-		title: '2+',
-		desctiption: 'Years of commercial development',
-	},
-	{
-		id: 1,
-		title: 4,
-		desctiption: 'Completed projects',
-	},
-	{
-		id: 2,
-		title: 'MicCheck',
-		desctiption: 'My current company',
-	},
+        id: 1,
+        title: '2+',
+        desctiption: 'Years experience',
+    },
 ];
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,13 @@ import cn from 'classnames';
 const Home: NextPage = () => {
     return (
         <Page title='Dennis Pavlyuk' description='Full-Stack Developer'>
-            <div className={cn(styles.overlay, styles.wrapper)}>
-                {/* <div className={cn(styles.div, styles.div1)}></div>
+{/*             <div className={cn(styles.overlay, styles.wrapper)}>
+                <div className={cn(styles.div, styles.div1)}></div>
                 <div className={cn(styles.div, styles.div2)}></div>
                 <div className={cn(styles.div, styles.div3)}></div>
                 <div className={cn(styles.div, styles.div4)}></div>
-                <div className={cn(styles.div, styles.div5)}></div> */}
-            </div>
+                <div className={cn(styles.div, styles.div5)}></div>
+            </div> */}
             <Header />
             <Main />
             <Footer />
